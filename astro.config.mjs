438fa816ec1ea66658d1f9456dfec4c85edbd6a6
edward <lxfg_cn@163.com>
2023-04-13 16:05:36 +0800

    astro.con

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,21 @@
-import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/edge"
-import node from "@astrojs/node"
-import netlify from "@astrojs/netlify/edge-functions"
-import cloudflare from "@astrojs/cloudflare"
-import unocss from "unocss/astro"
-import {
-  presetUno,
-  presetIcons,
-  presetAttributify,
-  presetTypography
-} from "unocss"
-import solidJs from "@astrojs/solid-js"
-import AstroPWA from "@vite-pwa/astro"
+import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
+import solidJs from '@astrojs/solid-js'
 
-const adapter = () => {
-  if (process.env.VERCEL) {
+import node from '@astrojs/node'
+import { VitePWA } from 'vite-plugin-pwa'
+import vercel from '@astrojs/vercel/edge'
+import netlify from '@astrojs/netlify/edge-functions'
+import disableBlocks from './plugins/disableBlocks'
+
+const envAdapter = () => {
+  if (process.env.OUTPUT === 'vercel') {
     return vercel()
-  } else if (process.env.NETLIFY) {
+  } else if (process.env.OUTPUT === 'netlify') {
     return netlify()
-  } else if (process.env.CF_PAGES) {
-    // cloudflare 无法提供 node18 环境，所以目前无法正常运行。
-    return cloudflare()
   } else {
     return node({
-      mode: "standalone"
+      mode: 'standalone',
     })
   }
 }
@@ -31,66 +23,51 @@ const adapter = () => {
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
-        presetTypography({
-          cssExtend: {
-            ":not(pre) > code::before,:not(pre) > code::after": ""
-          }
-        }),
-        presetIcons()
-      ]
-    }),
+    unocss(),
     solidJs(),
-    AstroPWA({
-      base: "/",
-      scope: "/",
-      includeAssets: ["favicon.svg"],
-      registerType: "autoUpdate",
-      manifest: {
-        name: "ChatGPT",
-        lang: "zh-cn",
-        short_name: "ChatGPT",
-        background_color: "#f6f8fa",
-        icons: [
-          {
-            src: "192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "256.png",
-            sizes: "256x256",
-            type: "image/png"
-          },
-          {
-            src: "512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      },
-      disable: !!process.env.NETLIFY,
-      workbox: {
-        navigateFallback: "/404",
-        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"]
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\/404$/]
-      }
-    })
   ],
-  output: "server",
-  adapter: adapter(),
+  output: 'server',
+  adapter: envAdapter(),
   vite: {
+    plugins: [
+      process.env.OUTPUT === 'vercel' && disableBlocks(),
+      process.env.OUTPUT === 'netlify' && disableBlocks('netlify'),
+      process.env.OUTPUT !== 'netlify' && VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'ChatGPT-API Demo',
+          short_name: 'ChatGPT Demo',
+          description: 'A demo repo based on OpenAI API',
+          theme_color: '#212129',
+          background_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'icon.svg',
+              sizes: '32x32',
+              type: 'image/svg',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        client: {
+          installPrompt: true,
+          periodicSyncForUpdates: 20,
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
     build: {
       chunkSizeWarningLimit: 1600,
     },
