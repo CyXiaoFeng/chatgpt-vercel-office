a904378b8b7bad5bacb4e47c57553f3c29aadc1d
edward <lxfg_cn@163.com>
2023-04-14 11:35:17 +0800

    change astro.config

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -53,7 +53,7 @@ export const post: APIRoute = async context => {
       { status: 401 }
     )
   }
-  const initOptions = generatePayload("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", messages)
+  const initOptions = generatePayload("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", messages)
   // #vercel-disable-blocks
   if (httpsProxy) initOptions.dispatcher = new ProxyAgent(httpsProxy)
   // #vercel-end
