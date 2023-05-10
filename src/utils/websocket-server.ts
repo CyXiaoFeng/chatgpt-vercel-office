5d1fac08833e4da31d6902b3df8d7f7a63e12b8b
edward <lxfg_cn@163.com>
2023-05-10 14:27:51 +0800

    增加astro.config.mjs中初始化socket

--- src/utils/websocket-server.ts
+++ src/utils/websocket-server.ts
@@ -2,39 +2,29 @@ import { WebSocketServer,WebSocket } from "ws"
 import { platform } from 'node:process'
 import { createServer } from 'https'
 import { readFileSync } from 'fs'
+let ws: WebSocket | null = null
+let wss: WebSocketServer
 
 if (platform === 'win32') {
   console.log('init websocket for Windows')
-  const server = createServer({
-    cert: readFileSync('G:\\ssl\\localhost.crt'),
-    key: readFileSync('G:\\ssl\\localhost.key')
+  const server = createServer()
+  const wss = new WebSocketServer({ noServer: true })
 
+  wss.on('connection', function connection(ws, request) {
+    ws.on('error', console.error)
+
+    ws.on('message', function message(data) {
+      console.log(`Received message ${data} from user `)
+      ws.send("Have already received the message")
+    })
   })
-  const wss = new WebSocketServer({ server })
+  server.on('upgrade', function upgrade(request, socket, head) {
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error)
-
-  ws.on('message', function message(msg) {
-    console.log(msg.toString())
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request)
+    })
   })
-})
-server.listen(3232,function listening() {
-  const url = `wss://192.168.3.211:${server.address().port}`
-  console.info(url)
-  const ws = new WebSocket(url, {
-    rejectUnauthorized: false,
-    perMessageDeflate: false,
-    secureProtocol: 'TLSv1_2_method'
-  })
-
-  ws.on('error', console.error)
-
-  ws.on('open', function open() {
-    ws.send('All glory to WebSockets!')
-  })
-
-})
+  server.listen(3232)
 
 } else {
   console.log('init websocket for linux')
@@ -76,4 +66,7 @@ export const send = (msg: string) => {
     console.info(`send msg=${msg}`)
     ws.send(msg)
   }
+  // const arrIterator = WSS.clients.values()
+  // const arr = Array.from(arrIterator)
+  // console.log(arr.length)
 }
