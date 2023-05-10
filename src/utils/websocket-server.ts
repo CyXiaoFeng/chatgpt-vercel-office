9d1d89392f7cc054a35daef94ad58b25eb1fc8bc
edward <lxfg_cn@163.com>
2023-05-10 14:19:29 +0800

    增加astro.config.mjs中初始化socket

--- src/utils/websocket-server.ts
+++ src/utils/websocket-server.ts
@@ -3,45 +3,42 @@ import { platform } from 'node:process'
 import { createServer } from 'https'
 import type { Server } from 'https'
 import { readFileSync } from 'fs'
-import * as dotenv from 'dotenv'
-declare global {
-  let ws:WebSocket
-  let server:Server
-  let wss:WebSocketServer
-}
-
+let ows
+let server:Server
 export const initsocket = () => {
   if (platform === 'win32') {
     console.log('init websocket for Windows')
-    global.server = createServer({
+     server = createServer({
       cert: readFileSync('G:\\ssl\\localhost.crt'),
       key: readFileSync('G:\\ssl\\localhost.key')
     })
 
   } else {
     console.log('init websocket for linux')
-    global.server = createServer({
+     server = createServer({
       cert: readFileSync('/etc/ssl/aichut/certificate.pem'),
       key: readFileSync('/etc/ssl/aichut/private.pem')
+
     })
   }
-    global.wss = new WebSocketServer({ server:global.server })
+    const wss = new WebSocketServer({ server })
     wss.on('connection', function connection(ws) {
-      global.ws = ws
+      ows = ws
       ws.on('error', console.error)
+
       ws.on('message', function message(msg) {
         console.log(msg.toString())
       })
     })
     server.listen(3232, function listening() {
-      dotenv.config()
-      const url = process.env.WEBSOCKET_SSL_URI
+      const url = `wss://localhost:${server.address().port}`
       console.info(url)
-      global.ws = new WebSocket(url, {
+      const ws = new WebSocket(url, {
         rejectUnauthorized: false,
         perMessageDeflate: false,
         secureProtocol: 'TLSv1_2_method'
       })
+      ows = ws
       ws.on('error', console.error)
       ws.on('open', function open() {
         ws.send('All glory to WebSockets!')
@@ -50,9 +47,10 @@ export const initsocket = () => {
 
 }
 export const send = (msg: string) => {
-  if (global.ws !== null || global.ws !== undefined) {
+  if (ows !== null) {
     console.info(`send msg=${msg}`)
-    global.ws.send(msg)
+    console.info
+    ows.send(msg)
   }
 }
 
