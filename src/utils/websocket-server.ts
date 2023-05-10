20f2a98dc8e1f5e0719c8222263e1a6c99b024eb
edward <lxfg_cn@163.com>
2023-05-10 14:53:26 +0800

    在websocket中增加全局ws

--- src/utils/websocket-server.ts
+++ src/utils/websocket-server.ts
@@ -2,40 +2,39 @@ import { WebSocketServer, WebSocket } from "ws"
 import { platform } from 'node:process'
 import { createServer } from 'https'
 import { readFileSync } from 'fs'
+declare global {
+  let wss:WebSocketServer
+  let ws:WebSocket
+}
 export const initsocket = ()=>{
-const server = createServer({
-  cert: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.crt':'/etc/ssl/aichut/certificate.pem'),
-  key: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.key':'/etc/ssl/aichut/private.pem')
-
-})
-  const wss = new WebSocketServer({ server })
-
-  wss.on('connection', function connection(ws) {
-    ws.on('error', console.error)
-
-    ws.on('message', function message(msg) {
-      console.log(msg.toString())
-    })
+  const server = createServer({
+    cert: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.crt':'/etc/ssl/aichut/certificate.pem'),
+    key: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.key':'/etc/ssl/aichut/private.pem')
   })
-  server.listen(3232, function listening() {
-    const url = `wss://localhost:${server.address().port}`
-    console.info(url)
-    const ws = new WebSocket(url, {
-      rejectUnauthorized: false,
-      perMessageDeflate: false,
-      secureProtocol: 'TLSv1_2_method'
+    wss = new WebSocketServer({ server })
+    wss.on('connection', function connection(ws) {
+      global.ws = ws
+      ws.on('error', console.error)
+      ws.on('message', function message(msg) {
+        console.log(msg.toString())
+      })
     })
-
-    ws.on('error', console.error)
-
-    ws.on('open', function open() {
-      ws.send('All glory to WebSockets!')
+    server.listen(3232, function listening() {
+      const url = `wss://localhost:${server.address().port}`
+      console.info(url)
+      const ws = new WebSocket(url, {
+        rejectUnauthorized: false,
+        perMessageDeflate: false,
+        secureProtocol: 'TLSv1_2_method'
+      })
+      ws.on('error', console.error)
+      ws.on('open', function open() {
+        ws.send('All glory to WebSockets!')
+      })
     })
-
-  })
 }
 export const send = (msg: string) => {
-  if (ws !== null) {
+  if (global.ws !== null) {
     console.info(`send msg=${msg}`)
     ws.send(msg)
   }
