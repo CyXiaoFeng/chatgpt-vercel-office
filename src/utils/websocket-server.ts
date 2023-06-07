54f96df0a7e6311b8d856ddcb9c8c7fd298bd48d
edward <lxfg_cn@163.com>
2023-06-07 17:13:03 +0800

    change ssl pem location

--- src/utils/websocket-server.ts
+++ src/utils/websocket-server.ts
@@ -1,42 +1,48 @@
 import { WebSocketServer, WebSocket } from "ws"
-import { platform } from 'node:process'
-import { createServer } from 'https'
-import { readFileSync } from 'fs'
-let lws:WebSocket
+import { platform } from "node:process"
+import { createServer } from "https"
+import { readFileSync } from "fs"
+let lws: WebSocket
 global.listnerServer
 // export const initsocket = ()=>{
-  const server = createServer({
-    cert: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.crt':'/etc/ssl/aichut/certificate.pem'),
-    key: readFileSync(platform === 'win32'?'G:\\ssl\\localhost.key':'/etc/ssl/aichut/private.pem')
+const server = createServer({
+  cert: readFileSync(
+    platform === "win32"
+      ? "G:\\ssl\\localhost.crt"
+      : "../aichut.com/certificate.pem"
+  ),
+  key: readFileSync(
+    platform === "win32"
+      ? "G:\\ssl\\localhost.key"
+      : "../aichut.com/private.pem"
+  )
+})
+const wss = new WebSocketServer({ server })
+wss.on("connection", function connection(ws) {
+  lws = ws
+  ws.on("error", console.error)
 
+  ws.on("message", function message(msg) {
+    console.log(msg.toString())
   })
-    const wss = new WebSocketServer({ server })
-    wss.on('connection', function connection(ws) {
-      lws = ws
-      ws.on('error', console.error)
-
-      ws.on('message', function message(msg) {
-        console.log(msg.toString())
-      })
+})
+if (!global.listnerServer) {
+  global.listnerServer = server.listen(3232, function listening() {
+    const url = `wss://localhost:${server.address().port}`
+    console.info(url)
+    const ws = new WebSocket(url, {
+      rejectUnauthorized: false,
+      perMessageDeflate: false,
+      secureProtocol: "TLSv1_2_method"
     })
-    if(!global.listnerServer) {
-      global.listnerServer = server.listen(3232, function listening() {
-      const url = `wss://localhost:${server.address().port}`
-      console.info(url)
-      const ws = new WebSocket(url, {
-        rejectUnauthorized: false,
-        perMessageDeflate: false,
-        secureProtocol: 'TLSv1_2_method'
-      })
 
-      ws.on('error', console.error)
+    ws.on("error", console.error)
 
-      ws.on('open', function open() {
-        ws.send('All glory to WebSockets!')
-      })
-
+    ws.on("open", function open() {
+      ws.send("All glory to WebSockets!")
     })
-  }
+  })
+}
 // }
 export const send = (msg: string) => {
   if (lws !== null && lws !== undefined) {
@@ -46,5 +52,4 @@ export const send = (msg: string) => {
     console.info("web socket not ready")
     // initsocket()
   }
-
 }
